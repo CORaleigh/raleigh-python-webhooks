ceeb15bf57832b61d8ec1f08511f126a82756acf
justingreco <justin.greco@raleighnc.gov>
2017-03-16 22:31:55 -0400

    testing github heroku integration

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-    return "Test"
     print("Request:")
     print(json.dumps(req, indent=4))
     if req.get('result').get('action') != 'find_events':
